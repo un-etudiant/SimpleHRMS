f3171eb94081adc0f4c6e68a6226cdb0fe2fa764
un-etudiant <pradeepssundaram@outlook.com>
2024-08-04 16:05:58 +0530

    Initial Commit

---
+++ src/main/java/com/hrms/samplehrms/SampleHrmsApplication.java
@@ -0,0 +1,13 @@
+package com.hrms.samplehrms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleHrmsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SampleHrmsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hrms/samplehrms/SampleHrmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hrms.samplehrms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SampleHrmsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
